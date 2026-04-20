31e63ee45862dfe2b4ffd7a4045d438c311dcfd3
Gavin Lofts <gavin5342@gmail.com>
2026-04-20 09:16:35 +0100

    build slides linux

---
+++ build_slides.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pandoc ./docs/slide_src/1_qproject/slides.md -t revealjs -s --standalone -V revealjs-url=https://unpkg.com/reveal.js@5 -V theme=white --css=../../slides.css -o ./docs/dist/1_qproject/slides.html
+pandoc ./docs/slide_src/index.md -t revealjs -s --standalone -V revealjs-url=https://unpkg.com/reveal.js@5 -V theme=white --css=./slides.css -o ./docs/index.html
